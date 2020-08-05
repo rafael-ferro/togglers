aefa6efbbdf23befb8d4826b4d84c87369b1ab89
Rafael Ferro <rafael.minatogau.ferro@gmail.com>
2020-08-05 14:07:37 -0300

    Adapt command to toggle wifi

--- toggle-wifi.sh
+++ toggle-wifi.sh
@@ -3,7 +3,9 @@
 # https://askubuntu.com/questions/1144596/set-a-shortcut-to-toggle-bluetooth-on-or-off
 
 if rfkill list wlan | grep -q 'yes$' ; then
-    rfkill unblock wlan
+  nmcli radio wifi on
+  notify-send -i /usr/share/icons/Pop/scalable/status/network-wireless-connected-symbolic.svg -h int:transient:1 "Wifi" "Ligando wifi..."
 else
-    rfkill block wlan
+  nmcli radio wifi off
+  notify-send -i /usr/share/icons/Pop/scalable/status/network-wireless-disconnected-symbolic.svg -h int:transient:1 "Wifi" "Desligando wifi..."
 fi
